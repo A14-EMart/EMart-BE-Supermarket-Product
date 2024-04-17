5e86014ecc262c38c0d9399fdbe8edfd19b1612e
Rayhan Adhisworo Jardine H <rayhan.jardine@gmail.com>
2024-04-17 11:51:51 +0700

    Initial commit.

---
+++ src/main/java/com/a14/emart/backendsp/BackendSpApplication.java
@@ -0,0 +1,13 @@
+package com.a14.emart.backendsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpApplication.class, args);
+	}
+
+}
