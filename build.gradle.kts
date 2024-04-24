1f8710c5572e6d7e111a6a94c69d3ceda6b84356
KenBalya <124903836+KenBalya@users.noreply.github.com>
2024-04-24 14:55:30 +0700

    Re-attempt GCD deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.2.4"
 	id("io.spring.dependency-management") version "1.1.4"
+	jacoco
 }
 
 group = "com.a14.emart"
@@ -33,4 +34,21 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.test {
+	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport) //to always generate report after tests
+}
+
+tasks.jacocoTestReport {
+	classDirectories.setFrom(files(classDirectories.files.map{
+		fileTree(it) {exclude("**/*Application**")}
+	}))
+	dependsOn(tasks.test)
+	reports {
+		xml.required.set(false)
+		csv.required.set(false)
+		html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+	}
 }
