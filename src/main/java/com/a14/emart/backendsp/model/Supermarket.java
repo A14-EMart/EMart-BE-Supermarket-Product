53c5e4ac6363af42df466dacecfa8a31ed2e593d
KenBalya <124903836+KenBalya@users.noreply.github.com>
2024-04-24 18:37:14 +0700

    Test Supermarket model (Red)

---
+++ src/main/java/com/a14/emart/backendsp/model/Supermarket.java
@@ -0,0 +1,38 @@
+package com.a14.emart.backendsp.model;
+
+import org.hibernate.annotations.GenericGenerator;
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "supermarket")
+public class Supermarket {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false)
+    @Lob
+    private String description;
+
+    @Column(nullable = false)
+    private String pengelola;
+
+    public Supermarket() {
+        // Default constructor
+    }
+
+    public Supermarket(String name, String description, String pengelola) {
+        this.name = name;
+        this.description = description;
+        this.pengelola = pengelola;
+    }
+
+
+}
