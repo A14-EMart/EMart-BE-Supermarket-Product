0c922431e7bbb50a4a8506d545be5394f1347b27
sunflawlxs <sherylivana99@gmail.com>
2024-05-01 20:42:56 +0700

    [GREEN] Create implementation and interface for ProductService

--- src/main/java/com/a14/emart/backendsp/service/ProductService.java
+++ src/main/java/com/a14/emart/backendsp/service/ProductService.java
@@ -1,4 +1,12 @@
 package com.a14.emart.backendsp.service;
 
-public class ProductService {
+import com.a14.emart.backendsp.model.Product;
+import java.util.List;
+
+public interface ProductService {
+    public Product createProduct(String name, Long price, Integer stock);
+    public Product editProduct(String UUID, Product changeAttribute);
+    public Product deleteProduct(String UUID);
+    public List<Product> getAllProduct();
+    public List<Product> searchProduct(String name);
 }
