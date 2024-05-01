26ff000321fb80c42d856ec3a84ad3eca4fc7ae9
sunflawlxs <sherylivana99@gmail.com>
2024-05-01 20:50:14 +0700

    [CONTROLLER] Created ProductController to send all product API

---
+++ src/main/java/com/a14/emart/backendsp/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.a14.emart.backendsp.controller;
+
+import com.a14.emart.backendsp.model.Product;
+import com.a14.emart.backendsp.service.ProductService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/all-product")
+    public ResponseEntity<List<Product>> allProduct() {
+        List<Product> allProduct = productService.getAllProduct();
+        return ResponseEntity.ok(allProduct);
+    }
+
+    @GetMapping("/search/{query}")
+    public ResponseEntity<List<Product>> queryProduct(String query) {
+        List<Product> queryProduct = productService.searchProduct(query);
+        return ResponseEntity.ok(queryProduct);
+    }
+}
---
+++ src/main/java/com/a14/emart/backendsp/dto/GetProfileResponse.java
@@ -0,0 +1,9 @@
+package com.a14.emart.backendsp.dto;
+
+public class GetProfileResponse
+{
+    public Long id;
+    public String email;
+    public String name;
+    public String role;
+}
