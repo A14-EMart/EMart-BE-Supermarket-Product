27943cad1947ea998c4a9cd8760e1a3f0c6358fa
sunflawlxs <sherylivana99@gmail.com>
2024-05-01 21:00:31 +0700

    [DTO] Created DTO for API mapping

---
+++ src/main/java/com/a14/emart/backendsp/dto/CreateProductRequest.java
@@ -0,0 +1,9 @@
+package com.a14.emart.backendsp.dto;
+
+public class CreateProductRequest {
+    public String name;
+    public Integer stock;
+    public Long price;
+}
+
+
---
+++ src/main/java/com/a14/emart/backendsp/dto/ModifyProductResponse.java
@@ -0,0 +1,8 @@
+package com.a14.emart.backendsp.dto;
+
+public class ModifyProductResponse {
+    public String UUID;
+    public String name;
+    public Long price;
+    public Integer stock;
+}
