c02eae40f685efa648d0b94f6903c208a79ed7b8
sunflawlxs <sherylivana99@gmail.com>
2024-05-15 20:13:05 +0700

    [RED] interface for search product by name

--- src/main/java/com/a14/emart/backendsp/service/ProductService.java
+++ src/main/java/com/a14/emart/backendsp/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.a14.emart.backendsp.service;
 
 import com.a14.emart.backendsp.model.Product;
+import com.a14.emart.backendsp.model.Supermarket;
 import java.util.List;
 
 public interface ProductService {
@@ -8,6 +9,6 @@ public interface ProductService {
     public Product editProduct(String UUID, Product product);
     public Product deleteProduct(String UUID);
     public List<Product> getAllProduct();
-    public List<Product> searchProductByName(String name);
+    public List<Product> searchProductByName(Supermarket supermarket, String name);
     public Product searchProductById(String id);
 }
