ec0fddf34c7d056650c73810fc15e8de15ab4a93
sunflawlxs <sherylivana99@gmail.com>
2024-05-15 20:20:36 +0700

    [REFACTOR] implement search product by name

--- src/main/java/com/a14/emart/backendsp/service/ProductServiceImpl.java
+++ src/main/java/com/a14/emart/backendsp/service/ProductServiceImpl.java
@@ -1,85 +1,68 @@
 package com.a14.emart.backendsp.service;
-import com.a14.emart.backendsp.dto.DeleteProductRequest;
-import com.a14.emart.backendsp.dto.ModifyProductResponse;
 import lombok.RequiredArgsConstructor;
-import java.util.Optional;
-import com.a14.emart.backendsp.repository.*;
+import com.a14.emart.backendsp.repository.ProductRepository;
+import com.a14.emart.backendsp.repository.SupermarketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-import com.a14.emart.backendsp.model.ProductBuilder;
 import com.a14.emart.backendsp.model.Product;
+import com.a14.emart.backendsp.model.Supermarket;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
-import java.util.Map;
+import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
-    private static String AUTH_BASE_URL = "";
 
+    @Autowired
+    private SupermarketRepository supermarketRepository;
+    private static String AUTH_BASE_URL = "";
     private WebClient webClient;
 
-//    private String getRole() {
-//        GetProfileResponse response = webClient.get()
-//                .uri(AUTH_BASE_URL + "/api/user/profile",
-//                        uriBuilder -> uriBuilder.queryParam("email", managerId).build())
-//                .retrieve()
-//                .bodyToMono(GetProfileResponse.class)
-//                .block();
-//        System.out.println(response.role);
-//        return response.role;
-//    }
-
-//    public boolean isManager() {
-//        String role = getRole();
-//        return role.equals("MANAGER");
-//    }
-
-    public Product createProduct(Product product)  {
-        if (true) {
-            productRepository.save(product);
-            return product;
+    public Supermarket createProduct(Product product) {
+        if (product == null) {
+            throw new IllegalArgumentException();
         }
-        return null;
+        productRepository.save(product);
+        return supermarketRepository.save(product.getSupermarket());
     }
 
-    public Product editProduct(String UUID, Product product) {
-        if (true) {
-            System.out.println(UUID);
-            Product productModify = productRepository.findById(UUID).orElseThrow();
-            productModify.setName(product.getName());
-            productModify.setPrice(product.getPrice());
-            productModify.setStock(product.getStock());
-            productRepository.save(productModify);
-            return productModify;
-        }
-        return null;
+    public Product editProduct(String UUID, Product changeAttribute) {
+        Product product = productRepository.findById(UUID).orElseThrow();
+        product.setName(changeAttribute.getName());
+        product.setPrice(changeAttribute.getPrice());
+        product.setStock(changeAttribute.getStock());
+        return productRepository.save(product);
     }
 
     public Product deleteProduct(String UUID) {
-        if (true) {
-            Product product = productRepository.findById(UUID).orElseThrow();
-            productRepository.delete(product);
-            return product;
-        }
-        return null;
+        Product product = productRepository.findById(UUID).orElseThrow();
+        productRepository.delete(product);
+        return product;
     }
 
-    public List<Product> getAllProduct() {
-        List<Product> allProduct = productRepository.findAll();
+    @Override
+    public List<Product> getAllProduct(Long supermarket) {
+        List<Product> allProduct = supermarketRepository.getReferenceById(supermarket).getProducts();
         return allProduct;
     }
 
-    public List<Product> searchProductByName(String name) {
-        System.out.println(name);
-        List<Product> searchProductByName = productRepository.findAllByNameIsContainingIgnoreCase(name);
-        return searchProductByName;
+    public List<Product> searchProductByName(Supermarket supermarket, String name) {
+        List<Product> searchProductByName = supermarket.getProducts();
+        List<Product> productList = new ArrayList<>();
+        for (Product product : searchProductByName) {
+            if (product.getName().toLowerCase().contains(name.toLowerCase())) {
+                productList.add(product);
+            }
+        }
+        return productList;
     }
+
     public Product searchProductById(String id) {
         return productRepository.findById(id).orElseThrow();
     }
